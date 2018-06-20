1ebf9aeb78076dfd484c9ca113e95a24fe834738
Abdullah Mohammed <abdullah.mohammed@maf.co.ae>
2018-06-20 17:16:49 +0300

    build

--- src/Cybersource/Traits/CybersourceValidatorTrait.php
+++ src/Cybersource/Traits/CybersourceValidatorTrait.php
@@ -5,7 +5,7 @@ namespace Cybersource\Traits;
 trait CybersourceValidatorTrait {
 	
     /**
-     * $_addCardAction shows the correct structure for AddCard Service Array
+     * $_redirectFormData shows the correct structure for AddCard Service Array
      * @var Array
      */
     protected $_redirectFormData = [
@@ -23,23 +23,11 @@ trait CybersourceValidatorTrait {
     ];
 
    
-    /**
-     * validateAddCardAction accepts the array to be validated fot AddCard Service
-     * it will pass the array it recieved along with it's correct structure that
-     * it should implement to the followsFormat function
-     * @param  Array  $data [array for AddCard Service]
-     * @return Bool       [whether it follows the format or not]
-     */
+    
     protected function validateRedirectFormData(array $data = []) : bool {
         return $this->followsFormat($data , $this->_redirectFormData);
     }
 
-    /**
-     * followsFormat checks if the passed array follows it's predefined structure
-     * @param  Array $data   [array of data for a particular service]
-     * @param  [Array] $format [array of predefined data structure for a particular service  ]
-     * @return [Bool]         [whether the match or not based on there keys matching recursively]
-     */
     protected function followsFormat($data , $format) : bool {
         if(array_keys($data) != array_keys($format))
             return false;
